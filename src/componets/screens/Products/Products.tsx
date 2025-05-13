32d01b2e94dcf93971877882bd68d34c5ccfc091
EmilioBaigorria <emiliobaigorriafacultad@gmail.com>
2025-05-13 18:36:14 -0300

    Crud para categorias completado y funcionando

--- src/componets/screens/Products/Products.tsx
+++ src/componets/screens/Products/Products.tsx
@@ -5,10 +5,16 @@ import { ProductListing } from "../../ui/ProductListing/ProductListing"
 import { Sidebar } from "../../ui/Sidebar/Sidebar"
 import styles from "./Products.module.css"
 
+
 export const Products = () => {
   const [productModal, setProductModal] = useState(false)
+  const test= async()=>{
+    const response=null
+    console.log(response)
+  }
   return (
     <div className={styles.mainContainer}>
+      <button onClick={test}>test</button>
       <ModalCrearEditarProducto isOpen={productModal} onClose={() => setProductModal(false)} />
       <div className={styles.headerContainer}>
         <Header />
